4921b3a9b4a9feeca5d1ebe8e7459699d529132c
Bart Widlarz <bartlomiejwidlarz@gmail.com>
2019-03-05 09:03:24 +0100

    Updating content licence

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,9 +63,7 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          Copyright © 2019 Bartłomiej Widlarz
         </footer>
       </div>
     )
