5dee5c64de3788f1eb3eb10d736565172db35d7d
Bart Widlarz <bartlomiejwidlarz@gmail.com>
2019-03-25 08:40:02 +0100

    Installing autolink headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,13 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-prismjs`,
+          'gatsby-remark-autolink-headers',
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              inlineCodeMarker: '÷',
+            },
+          },
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
         ],
