2b615e2b29c6565810a082eb7dbd5279655eff4c
Bart Widlarz <bartlomiejwidlarz@gmail.com>
2019-05-05 13:41:55 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-876a3bbee13ee3ec4fd6.js"
+    "url": "webpack-runtime-0ae84deda46c4ac50b8e.js"
   },
   {
     "url": "styles.8ebf7344c6aea24359b2.css"
@@ -35,14 +35,14 @@ self.__precacheManifest = [
     "url": "styles-18762bd86cf8b703ea89.js"
   },
   {
-    "url": "app-07fcdfd476f94268ff3c.js"
+    "url": "app-8867a1619dac3543ffaf.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-60c40c444802a8e3e288.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "219ae73de0ff4df98473ea0dfb1b86e8"
+    "revision": "fcc08bb19ffe5abb9a9f93b20746b95a"
   },
   {
     "url": "1-29b465489ea9bd93535e.js"
